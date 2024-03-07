f5b254114938e95bd08c2db165b43593235df826
Shaik Anees Ahammed <shaik.nafees.2789@gmail.com>
2024-03-08 02:51:59 +0530

    Added the random testimonials to project

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,26 +173,26 @@ const experiences = [
 const testimonials = [
     {
         testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.",
+        name: "Random Name",
+        designation: "XYZ",
+        company: "ABC Co",
         image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
         testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.",
+        name: "Random Name",
+        designation: "XYZ",
+        company: "ABC Corp",
         image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
         testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.",
+        name: "Random Name",
+        designation: "XYZ",
+        company: "ABC Enterprises",
         image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
 ];
